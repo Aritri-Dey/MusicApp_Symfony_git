f5d97e2efa0c8cf0b6ed148db86eae415e607399
Aritri Dey <aritri.dey@innoraft.com>
2023-04-13 13:18:16 +0530

    Latest commit: Implementation of correct indentation.

--- src/Services/ValidMail.php
+++ src/Services/ValidMail.php
@@ -7,10 +7,22 @@ use App\Services\CheckMail;
  * This class is used to validate all form fields in the update form.
  */
 class ValidMail  {
+
+  /**
+   * @var string $ mail
+   *    Stores the mail to be checked.
+   */
   private $mail;
   function __construct(string $mail) {
     $this->mail = $mail;
   }
+
+  /**
+   * Function to check validity of mail ID by calling CheckMail class.
+   * 
+   *  @return string
+   *    Returns error message if validation is not successful.
+   */
   function validMail() {
     $mailObj = new CheckMail($this->mail);
       $flag = $mailObj->check();
